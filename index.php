fe053908b4673378b10427e5fb54543cf0b27d81
Gary Hepting <gary@squareup.com>
2014-06-19 20:17:14 -0700

    removed php files

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php require_once("includes/header.php"); ?>
-<img class="frame" src="images/Orange-Cloud.jpg" alt="Orange Cloud by Stephanie Robison" />
-<div class="clear"></div>
-<?php require_once("includes/footer.php"); ?>
